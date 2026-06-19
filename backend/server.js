278407342c1cf755c57407cde1ee2b05a2744449
syamjith <syamjithloq@gmail.com>
2026-06-19 08:13:54 +0530

    updated the remove err

--- backend/server.js
+++ backend/server.js
@@ -6,33 +6,6 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("MongoDB Connected");
-    })
-    .catch(err => {
-        console.log(err);
-    });
-
-app.use("/api/auth", require("./routes/auth"));
-
-const watchlistRoutes =
-    require("./routes/watchlist");
-
-app.use(
-    "/api/watchlist",
-    watchlistRoutes
-);
-
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
 app.use(cors({
   origin: [
     "https://filmfy.netlify.app",
@@ -42,8 +15,24 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.use("/api/auth", require("./routes/auth"));
+
+const watchlistRoutes = require("./routes/watchlist");
+app.use("/api/watchlist", watchlistRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
